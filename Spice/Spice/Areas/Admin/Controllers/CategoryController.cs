a0476b233130bfa46bae7f38357b81277f6651d4
Bart Klaasse <bart@bartklaasse.nl>
2019-11-14 20:02:33 +0100

    68 Add category controller and get action method

---
+++ Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        // Use Dependency injection to create our ApplicationDbContext
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        //Get Categories and return all categories to the view
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
