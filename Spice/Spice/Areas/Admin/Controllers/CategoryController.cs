76eb51aa859cc3fbecd2c4a696c87e6d8bc16cef
Bart Klaasse <bart@bartklaasse.nl>
2019-12-17 20:17:23 +0100

    78 Edit category edit action

--- Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -48,5 +48,24 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
 
         }
+
+        // Get method - Edit category 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // Post method - Edit Category
+
     }
 }
