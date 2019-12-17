9a7eab0406021ea5d4131262f3c0e858c364b561
Bart Klaasse <bart@bartklaasse.nl>
2019-12-17 21:08:52 +0100

    84 Delete Category post action

--- Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -96,5 +96,24 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
         }
 
+        // Post method - delete category
+        // Deze HttpPost krijgt een actionname attribuut omdat beide action methods niet dezelfde naam kunnen hebben
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed (int id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return View();
+            }
+
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
