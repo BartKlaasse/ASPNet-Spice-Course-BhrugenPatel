87a7cc9acb844bd31ba8182b4829ba17ce290e32
Bart Klaasse <bart@bartklaasse.nl>
2020-02-13 22:02:57 +0100

    96 get subcategory json

--- Spice/Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models;
@@ -74,5 +75,16 @@ namespace Spice.Areas.Admin.Controllers
             return View(modelVM);
         }
 
+        [ActionName("GetSubCategory")]
+        public async Task<IActionResult> GetSubCategory(int id)
+        {
+            List<SubCategory> subCategories = new List<SubCategory>();
+
+            subCategories = await (from subCategory in _db.SubCategory where subCategory.CategoryId == id select subCategory).ToListAsync();
+
+            return Json(new SelectList(subCategories, "Id", "Name"));
+
+        }
     }
+
 }
