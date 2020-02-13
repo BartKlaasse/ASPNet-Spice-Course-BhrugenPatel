8095c650e07d7d4b3dbbc41e2c60a754ba6bd4bc
Bart Klaasse <bart@bartklaasse.nl>
2020-02-13 21:24:12 +0100

    94 Sub cat post create

--- Spice/Spice/Extensions/IEnumerableExtension.cs
+++ Spice/Spice/Extensions/IEnumerableExtension.cs
@@ -10,8 +10,7 @@ namespace Spice.Extensions
     {
         public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
         {
-            return from item in items
-            select new SelectListItem
+            return from item in items select new SelectListItem
             {
                 Text = item.GetPropertyValue("Name"),
                     Value = item.GetPropertyValue("Id"),
