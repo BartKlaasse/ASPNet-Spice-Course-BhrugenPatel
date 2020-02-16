2586e79d308896f3dfe962f872769dc5754d618d
Bart Klaasse <bart@bartklaasse.nl>
2020-02-16 18:16:19 +0100

    108 menu item get create

--- Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -15,15 +16,31 @@ namespace Spice.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _hostingEnvironment;
 
+        // door gebruik te maken van de tag Bindpropery met daaronder de get-set hoef je het model niet meer als input te geven aan de method
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
         {
             _db = db;
             _hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
+
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        // Get - Create
+        // MenuItemVM word automatisch gebind aan de method
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
