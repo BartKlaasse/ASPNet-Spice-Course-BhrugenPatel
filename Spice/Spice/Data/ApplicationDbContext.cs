5fa26e096189770769f1ad9c3cfd43a267b2b7b9
Bart Klaasse <bart@bartklaasse.nl>
2020-02-17 19:08:03 +0100

    120 Add Coupon model

--- Spice/Spice/Data/ApplicationDbContext.cs
+++ Spice/Spice/Data/ApplicationDbContext.cs
@@ -9,11 +9,11 @@ namespace Spice.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Coupon> Coupon { get; set; }
     }
 }
