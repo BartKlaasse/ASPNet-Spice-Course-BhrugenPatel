41882ee374a2bbfafc536503d3a6a7e207c34835
Bart Klaasse <bart@bartklaasse.nl>
2020-02-17 12:49:54 +0100

    113 menu item create post p2

--- Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models.ViewModels;
+using Spice.Utility;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -43,7 +45,7 @@ namespace Spice.Areas.Admin.Controllers
             return View(MenuItemVM);
         }
         //Post - Create
-        [HttpPost]
+        [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreatePOST()
         {
@@ -67,12 +69,26 @@ namespace Spice.Areas.Admin.Controllers
             if (files.Count > 0)
             {
                 // file has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using(var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
             }
             else
             {
                 // no files have been uploaded. use default file
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
             }
+            await _db.SaveChangesAsync();
 
+            return RedirectToAction(nameof(Index));
         }
     }
 }
