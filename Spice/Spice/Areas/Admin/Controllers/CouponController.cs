a77b29b57ba0e5cf47c23f8899317daf46adc60d
Bart Klaasse <bart@bartklaasse.nl>
2020-02-19 15:37:48 +0100

    123 coupon create post method

--- Spice/Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CouponController.cs
@@ -26,5 +26,32 @@ namespace Spice.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupons)
+        {
+            if (ModelState.isValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using(var fs1 = files[0].OpenReadStream())
+                    {
+                        using(var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                _db.Coupon.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
+        }
     }
 }
