642e36ccbf4bb3fdd0dcd7b3c326a0ec80d12b37
Bart Klaasse <bart@bartklaasse.nl>
2020-02-24 11:21:54 +0100

    157 details post (cart)

--- Spice/Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Spice/Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -49,6 +50,51 @@ namespace Spice.Controllers
             };
 
             return View(cartObj);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ShoppingCart cartObj)
+        {
+            cartObj.Id = 0;
+            if (ModelState.IsValid)
+            {
+                var claimsIdentity = (ClaimsIdentity) this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                cartObj.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDB = await _db.ShoppingCart.Where(c => c.ApplicationUserId == cartObj.ApplicationUserId && c.MenuItemId == cartObj.MenuItemId).FirstOrDefaultAsync();
+                if (cartFromDB == null)
+                {
+                    // item not existing in cart
+                    await _db.ShoppingCart.AddAsync(cartObj);
+                }
+                else
+                {
+                    // item exists in cart already
+                    cartFromDB.Count = cartFromDB.Count + cartObj.Count;
+                }
+                await _db.SaveChangesAsync();
+
+                // display amount of items in cart via session
+                var count = _db.ShoppingCart.Where(c => c.ApplicationUserId == cartObj.ApplicationUserId).ToList().Count();
+                HttpContext.Session.SetInt32("sessionCartCount", count);
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                var menuItemFromDb = await _db.MenuItem.Include(c => c.Category).Include(s => s.SubCategory).Where(m => m.Id == cartObj.MenuItemId).FirstOrDefaultAsync();
+
+                ShoppingCart cartobject = new ShoppingCart()
+                {
+                    MenuItem = menuItemFromDb,
+                    MenuItemId = menuItemFromDb.Id
+                };
+
+                return View(cartobject);
+            }
         }
 
         public IActionResult Privacy()
